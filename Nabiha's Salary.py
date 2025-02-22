3dfa81a6402f31f903c56fd7ac4eccc9d08f418a
Bernard <bernardyoussef123@gmail.com>
2025-02-21 17:42:18 -0800

    calculation-functions

--- Nabiha's Salary.py
+++ Nabiha's Salary.py
@@ -11,3 +11,34 @@ def percentages() :
     rent_percentage = float(input("Enter the percentage for rent : "))
     electricity_percentage = float(input("Enter the percentage for electricity : ")) 
     return saving_percentage , rent_percentage , electricity_percentage
+
+def calculateAllocations() :
+    savings = salary * (saving_percentage / 100 )
+    rent = salary * (rent_percentage / 100 )
+    electricity = salary * (electricity_percentage / 100 ) 
+    return savings , rent , electricity
+
+def calculateRemainder():
+    total_expenses = savings + rent + electricity
+    remainder = salary - total_expenxses
+    return total_expenses , remainder
+
+def yearlyCosts() :
+       yearly_rent = rent * 12
+       yearly_electricity = electricity * 12
+       return  yearly_rent , yearly_electricity
+
+def funSalary() : 
+     funsalary = salary ** 2
+     return funsalary
+
+def savingsDivision() : 
+    additional_savings = 50
+
+    if savings > 0 :
+       after_division =   additional_savings / savings
+    else :
+      after_division  = float('inf')
+    return after_division
+
+
